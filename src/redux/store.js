f883634eb961fc5891243a1ebcc9020268662d5b
sodojiwo <alvinadetya@rocketmail.com>
2018-10-21 09:03:49 +0700

    action

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,41 @@
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 
 const reduxStore = () => {
-  const reducer = function(state, action) {
+  const stateUser = {
+    name: 'Alvin'
+  };
+  const userName = (state = stateUser, action) => {
+    switch (action.type) {
+      case 'CHANGE_NAME': {
+        state = { ...state, name: action.payload };
+        break;
+      }
+      case 'CHANGE_AGE': {
+        state = { ...state, age: action.payload };
+        break;
+      }
+      default: {
+        return state;
+      }
+    }
     return state;
   };
 
-  const store = createStore(reducer, { name: 'alvin' });
+  const reducer = combineReducers({
+    user: userName
+  });
+
+  const tweetsAction = () => {};
+
+  const store = createStore(reducer);
 
   store.subscribe(() => {
     console.log('store change', store.getState());
   });
 
-  store.dispatch({ type: 'INC', payload: 1 });
+  store.dispatch({ type: 'CHANGE_NAME', payload: 'ALVIN' });
+  store.dispatch({ type: 'CHANGE_AGE', payload: 18 });
+  store.dispatch({ type: 'CHANGE_NAME', payload: 'ADETYA' });
 };
 
 export default reduxStore;
